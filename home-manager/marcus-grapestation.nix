5352ed9318ef605ec9e3b090c16a26682de63fb8
grapeofwrath <69535018+grapeofwrath@users.noreply.github.com>
2025-01-09 14:38:40 -0800

    musical files

--- home-manager/marcus-grapestation.nix
+++ home-manager/marcus-grapestation.nix
@@ -1,10 +1,10 @@
 {gVar, ...}: {
-    imports = [
-        ./modules/base
-        ./modules/desktop
-    ];
-    home = {
-        username = gVar.defaultUser;
-        file.".steam/steam/steam_dev.cfg".source = ./../assets/steam_dev.cfg;
-    };
+  imports = [
+    ./modules/base
+    ./modules/desktop
+  ];
+  home = {
+    username = gVar.defaultUser;
+    file.".steam/steam/steam_dev.cfg".source = ./modules/config/steam_dev.cfg;
+  };
 }
